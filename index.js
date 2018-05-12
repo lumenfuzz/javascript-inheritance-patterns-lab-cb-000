7d7ac0e5a01b55a22f7161b8da6c0663fb7fd6ba
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-12 20:03:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ function Shape() {
   }
   this.addToPlane = this.prototype.addToPlane
   this.prototype.move = (x,y) => {
-
+    this.position.x = x
+    this.position.y = y
   }
+  this.move = this.prototype.move
 }
 
 var shapessa = new Shape()
