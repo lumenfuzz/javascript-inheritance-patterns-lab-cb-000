1d1f1e54678a4baca8f63fb4088ccbe2907ee0e6
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-12 21:13:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,3 +40,8 @@ Polygon.prototype.perimeter = function() {
 Polygon.prototype.numberOfSides = function() {
   return this.sides.length
 }
+
+function Quadrilateral(s1,s2,s3,s4) {
+  this.sides = [new Side(s1), new Side(s2), new Side(s3), new Side(s4)]
+}
+Quadrilateral.prototype = Object.create(Polygon.prototype)
