01717e737360baf69771f8e2b92d82924b5f70c9
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-12 19:40:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function Point(x,y) {
   this.x = x;
   this.y = y;
   this.prototype = Object.create(Object.prototype)
+  this.prototype.toString = `${this.x}, ${this.y}`
 }
 
 var pointessa = new Point(2,3)
