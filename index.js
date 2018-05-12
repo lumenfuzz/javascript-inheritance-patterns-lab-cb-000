8a678016c26f9b1322afaf590817604b0dde4637
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-12 21:01:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function Polygon(array) {
 }
 Polygon.prototype = Object.create(Shape.prototype)
 Polygon.prototype.perimeter = function() {
-  this.sides.reduce( (sum, e) => {
+  this.sides.reduce( function(sum, e) {
     return sum + e.length
   })
 }
